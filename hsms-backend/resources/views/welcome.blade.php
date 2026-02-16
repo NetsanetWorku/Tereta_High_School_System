eb69783a450d3ebe5f39976d03713b4d68e39a0d
Netsanet <netsanetworku143@gmail.com>
2026-02-16 08:07:08 +0300

    Add beautiful welcome message to frontend home page

--- hsms-backend/resources/views/welcome.blade.php
+++ hsms-backend/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>High School Management System - API</title>
+    <title>Tereta High School Management System - API</title>
     <style>
         * {
             margin: 0;
